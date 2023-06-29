b1e2bdf0ba764eb78aeaab7e59a0041caa338d1d
Andres Cordova <andrescordovaoficial@gmail.com>
2023-06-28 20:21:16 -0600

    Adding a new Footer Component. :sparkles:

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import React from "react";
+import "./footer.css";
+import { BsFacebook } from "react-icons/bs";
+import { BsTwitter } from "react-icons/bs";
 
 const Footer = () => {
   return (
-    <section id='footer'>Footer</section>
-  )
-}
+    <footer>
+      <a href="#home" className="footer__logo">
+        Cordova
+      </a>
+      <ul className="permalinks">
+        <li>
+          <a href="#home">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#experience">Experience</a>
+        </li>
+        <li>
+          <a href="#services">Services</a>
+        </li>
+        <li>
+          <a href="#portafolio">Portfolio</a>
+        </li>
+        <li>
+          <a href="#testimonials">Testimonials</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
 
-export default Footer
+      <div className="footer__socials">
+        <a href="https://www.facebook.com">
+          <BsFacebook />
+        </a>
+        <a href="https://www.twitter.com">
+          <BsTwitter />
+        </a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; Andres Cordova. All rights reserved.</small>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
